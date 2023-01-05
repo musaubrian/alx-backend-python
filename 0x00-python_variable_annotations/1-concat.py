ec19812d82f03105d17ff267e6db1ac94c07df8e
musaubrian <musaubrian15@gmail.com>
2023-01-05 18:59:59 +0300

    chore: return a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns a concatenated string
+    Arg::
+        str1(str)
+        str2(str)
+    """
+    return str1 + str2
