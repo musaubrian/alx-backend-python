2ea65dcfb1c53c798d510e4ce2350b8e4c2c2459
musaubrian <musaubrian15@gmail.com>
2023-01-05 18:49:21 +0300

    chore: return sum of two values

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Takes two values: floats adds returns their sum: float"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    returns sum of two values
+    Args::
+        a(float)
+        b(float)
+    """
+    return a + b
