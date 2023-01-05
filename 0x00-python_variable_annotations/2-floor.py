7b2976042cd33020d49f8e258d91b3a2d32c1b9e
musaubrian <musaubrian15@gmail.com>
2023-01-05 19:05:39 +0300

    chore: return floor of an argument

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Returns the floor of an argument"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of argument
+    Args::
+        n(float): argument to floor
+    """
+    return math.floor(n)
