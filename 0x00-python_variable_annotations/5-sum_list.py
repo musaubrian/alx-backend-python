2b2bf28cdcd60818fb65ba7f4d3fc54b91927407
musaubrian <musaubrian15@gmail.com>
2023-01-05 19:34:35 +0300

    chore: return sum of an array

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Returns sum of an array"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Returns sum of elements in an array
+    Args::
+        input_list(list)
+    """
+    return sum(input_list)
