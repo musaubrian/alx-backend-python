3c36171e8264d65bd6da2b8bd1dc4232ad86cbf1
musaubrian <musaubrian15@gmail.com>
2023-01-05 19:20:43 +0300

    chore: return string representation of an arg

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""returns the string representation of an argument"""
+
+
+def to_str(n: float) -> str:
+    """
+    return string representation of n
+    Args::
+        n(float)
+    """
+    return n.__str__()
