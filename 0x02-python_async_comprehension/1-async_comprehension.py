0b5a16f9a5671b9cf8b0b778b3cda989ba0a5ed1
musaubrian <musaubrian15@gmail.com>
2023-01-11 21:16:55 +0300

    chore: remove unused imports

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,8 +4,7 @@ The coroutine will collect 10 random numbers
 using an async comprehensing over async_generator,
 then return the 10 random numbers.
 """
-import asyncio
-from typing import AsyncGenerator, List
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
