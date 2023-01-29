3161958793416faf6db3b891ed7eaad088857b23
Ernest <musaubrian15@gmail.com>
2023-01-29 14:28:27 +0300

    chore: Implement TestMemoize(unittest.TestCase) class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Module contains test cases for the utils file"""
 
-from sys import exception
 from parameterized import parameterized
 from typing import Any, Dict, Mapping, Sequence
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import MagicMock, patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -21,14 +20,11 @@ class TestAccessNestedMap(unittest.TestCase):
     )
     def test_access_nested_map(
         self, nested_map: Mapping, path: Sequence, expected: Any
-    ):
+    ) -> None:
         """test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parametarized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError)
-    ])
+    @parametarized.expand([({}, ("a",), KeyError), ({"a": 1}, ("a", "b"), KeyError)])
     def test_access_nested_map_exception(self, nested_map: Mapping, path: Sequence):
         """test if KeyError is raised"""
         with self.assertRaises(KeyError) as cm:
@@ -51,3 +47,24 @@ class TestAccessNestedMap(unittest.TestCase):
             with patch("requests.get", return_value=Mock(**attr)) as get_r:
                 self.assertEqual(get_json(test_url), test_payload)
                 get_r.assert_called_once_with(test_url)
+
+    class TestMemoize(unittest.TestCase):
+        """Defines tests for the memoize function"""
+
+        def test_memoize(self) -> None:
+            """test the memoize function"""
+
+            class TestClass:
+                def a_method(self):
+                    return None
+
+                @memoize
+                def a_property(self):
+                    return self.a_method()
+
+            obj = TestClass()
+            obj.a_method = MagicMock(return_value=None)
+            result_1 = obj.a_property
+            result_2 = obj.a_property
+            self.assertEqual(result_1, result_2)
+            obj.a_method.assert_called_once()
