20334afbdc09a67933e3e67e739e3ab0ac7b8fdd
Ernest <musaubrian15@gmail.com>
2023-01-29 13:38:19 +0300

    chore: implement TestAccessNestedMap.test_access_nested_map_exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module contains test cases for the utils file"""
 
+from sys import exception
 from typing import Any, Mapping, Sequence
 from utils import access_nested_map
 import unittest
@@ -11,8 +12,8 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parametized.expand(
         [
-            ({"a": 1}, ("a"), 1),
-            ({"a": {"b": 2}}, ("a"), {"b": 2}),
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
             ({"a": {"b": 2}}, ("a", "b"), 2),
         ]
     )
@@ -21,3 +22,15 @@ class TestAccessNestedMap(unittest.TestCase):
     ):
         """test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parametized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(
+            self, nested_map: Mapping, path: Sequence):
+        """test if KeyError is raised"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+            exception = cm.exception
+            self.assertTrue(str(exception) not in path)
