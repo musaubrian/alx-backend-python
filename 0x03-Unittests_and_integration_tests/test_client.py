23b3e81dc2924da6a304c01dcf34e3af8abb5076
Ernest <musaubrian15@gmail.com>
2023-02-01 12:34:05 +0300

    chore: Implement TestGithubOrgClient.test_has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """unittests for the client package"""
 
+from typing import Dict
 import unittest
 from unittest.mock import PropertyMock, patch
 from parameterized import parameterized
@@ -42,11 +43,28 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_payload = [{"pay": "load1"}, {"pay2": "load2"}]
         mock_get_json.return_value = mock_payload
 
-        with patch("client.GithubOrgClient._public_repos_url",
-                   new_callable=PropertyMock ) as mock_public_repos_url):
+        with patch(
+            "client.GithubOrgClient._public_repos_url",
+            new_callable=PropertyMock
+        ) as mock_public_repos_url:
             mock_public_repos_url.return_value = "https://someurl.com"
             org_client = GithubOrgClient("some_org")
             result = org_client.public_repos()
 
             self.assertEqual(result, mock_payload)
             mock_public_repos_url.assert_called_once()
+
+    @parameterized.expand(
+        [
+            ({"license": {"key": "my_license"}}, "my_license", True),
+            ({"license": {"key": "other_license"}}, "my_license", False),
+        ]
+    )
+    def test_has_license(
+        self, repo: Dict[str, Dict], license_key: str, expected: bool
+    ) -> None:
+        """
+        unit-test for GithubOrgClient.has_license
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
